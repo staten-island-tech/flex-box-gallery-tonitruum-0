04aad0d04417c26363d7edddae6c824ea7f83973
tonitruum-0 <104451675+tonitruum-0@users.noreply.github.com>
2022-10-18 13:05:39 -0400

    final commit probably

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ let array = [];
 for (let i = 0; i < 9; i++) {
   array[i] = document.getElementById(`card-${i + 1}`);
 }
+const mediaQueryCondition = window.matchMedia("( max-width: 810px )");
+
 array.forEach((element) => {
   let currentElement = element;
-  element.addEventListener("mouseover", (e) => {
-    array.forEach((card) => {
-      if (!(card === currentElement)) {
-        card.classList.add("blur");
-        if (currentElement === array[4]) {
-          card.classList.add("middleBlur");
+  if (!mediaQueryCondition.matches) {
+    element.addEventListener("mouseover", (e) => {
+      array.forEach((card) => {
+        if (!(card === currentElement)) {
+          card.classList.add("blur");
+          if (currentElement === array[4]) {
+            card.classList.add("middleBlur");
+          }
         }
-      }
+      });
     });
-  });
-  currentElement.addEventListener("mouseout", (e) => {
-    array.forEach((cardpt2) => {
-      cardpt2.classList.remove("blur");
-      cardpt2.classList.remove("middleBlur");
+    currentElement.addEventListener("mouseout", (e) => {
+      array.forEach((cardpt2) => {
+        cardpt2.classList.remove("blur");
+        cardpt2.classList.remove("middleBlur");
+      });
     });
-  });
+  }
 });
